0b43d7868bc8c924904dd4e94b24b3b61c4dad88
Jason Heppler <jason.heppler@gmail.com>
2015-07-23 15:35:39 -0700

    Moving styles to inline

--- palladio-path-component.js
+++ palladio-path-component.js
@@ -6,41 +6,41 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 	.directive('palladioPathView', function (palladioService, dataService) {
 		return {
 			scope : {
-				
+
 			},
 			template : '<div id="main">' +
-						'<h2>Chinese Input &#8212; IN DEVELOPMENT</h2>' +
+						'<h3 style="font: sans-serif;">Chinese Input &#8212; IN DEVELOPMENT</h3>' +
 						'<div id="chart"></div>' +
 					'</div>',
 			link : {
 				pre : function(scope, element) {
 					scope.hardCodedData = hardCodedData();
-					
+
 				},
 
 				post : function(scope, element, attrs) {
 					// Anything that touches the DOM happens here.
-					
+
 					console.log(palladioService);
 					console.log(dataService);
-					
+
 					var xfilter = dataService.getDataSync().xfilter;
 					var fullData = dataService.getDataSync().data;
 					var dummyDim = xfilter.dimension(function(d) { return true; });
-					
+
 					var filteredData = dummyDim.top(Infinity);
-					
+
 					var m = d3.map();
 					fullData.sort(function(a,b){ return a.uniq > b.uniq; }).forEach(function(d) {
 						if(d.chinese) {
 							if(m.has([d.chinese, d.trial, d.session])) {
 								m.get([d.chinese, d.trial, d.session]).push(d.token);
 							} else {
-								m.set([d.chinese, d.trial, d.session], [d.token])
+								m.set([d.chinese, d.trial, d.session], [d.token]);
 							}
 						}
-					})
-					
+					});
+
 					var characters = [];
 					fullData
 						.sort(function(a,b){ return a.uniq > b.uniq; })
@@ -50,7 +50,7 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 								characters.push(d.chinese);
 							}
 						});
-					
+
 					var charkeys = d3.map();
 					m.entries().forEach(function(d){
 						if(charkeys.has(d.key.split(',')[0])){
@@ -60,9 +60,9 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 							graph.add(d.value);
 							charkeys.set(d.key.split(',')[0], graph);
 						}
-					})
-					
-					console.log(charkeys.entries().map(function(d) { 
+					});
+
+					console.log(charkeys.entries().map(function(d) {
 						var test;
 						try {
 							d.value = d.value.sort();
@@ -72,7 +72,7 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 						}
 						return d;
 					}));
-					
+
 					var width   = 960,
 					    height  = 200,
 					    margin  = 20,
@@ -80,41 +80,41 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					    padding = 10,
 					    radius  = 6,
 					    yfixed  = pad + radius;
-					
+
 					// Legend variables
 					var legend_x = 0,
 					    legend_y = 5,
 					    legend_width = 175,
 					    legend_height = 620,
-					    legend_margin = 20
+					    legend_margin = 20,
 					    key_y = 40,
 					    key_x = 16,
 					    mapped_y = legend_y + legend_height - 90;
-					
+
 					var color = d3.scale.category20();
-					
+
 					// Tooltip
 					var tooltip = d3.select("body").append("div")
 					  .classed("tooltip", true)
 					  .classed("hidden", true);
-					
+
 					// Main
 					//-----------------------------------------------------
 					function arcDiagram(graph) {
 					  var radius = d3.scale.sqrt()
 					    .domain([0, 20])
 					    .range([0, 15]);
-					
-					  var svg = d3.select("#chart").append("svg")
+
+					  var svg = d3.select(element[0]).append("svg")
 					      .attr("id", "arc")
 					      .attr("width", width)
 					      .attr("height", height);
-					
+
 					  // create plot within svg
-					  var plot = svg.append("g")
-					    .attr("id", "plot")
+					  var wrapper = svg.append("g")
+					    .attr("id", "wrapper")
 					    .attr("transform", "translate(" + padding + ", " + padding + ")");
-					
+
 					  // count the paths
 					  graph.links.forEach(function(d,i) {
 					    var pathCount = 0;
@@ -126,95 +126,97 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					    }
 					    d.pathCount = pathCount;
 					  });
-					
+
 					  // // Create the unique identifiers for the links
 					  // graph.links.forEach(function(d,i) {
-					
+
 					  // });
-					
+
 					  // fix graph links to map to objects
 					  graph.links.forEach(function(d,i) {
 					    d.source = isNaN(d.source) ? d.source : graph.nodes[d.source];
 					    d.target = isNaN(d.target) ? d.target : graph.nodes[d.target];
-					    d.sessions = ("Session" + d.session + "Trial" + d.trial + "Seg" + d.segment)
+					    d.sessions = ("Session" + d.session + "Trial" + d.trial + "Seg" + d.segment);
 					  });
-					
+
 					  linearLayout(graph.nodes);
 					  drawLinks(graph.links);
 					  drawNodes(graph.nodes);
 					}
-					
+
 					// layout nodes linearly
 					function linearLayout(nodes) {
 					  nodes.sort(function(a,b) {
 					    return a.uniq - b.uniq;
 					  });
-					
+
 					  var xscale = d3.scale.linear()
 					    .domain([0, nodes.length - 1])
 					    .range([radius, width - margin - radius]);
-					
+
 					  nodes.forEach(function(d, i) {
 					    d.x = xscale(i);
 					    d.y = yfixed;
 					  });
 					}
-					
+
 					function drawNodes(nodes) {
-					
-					  var gnodes = d3.select("#plot").selectAll("g.node")
+
+					  var gnodes = d3.select("#wrapper").selectAll("g.node")
 					    .data(nodes);
-					
+
 					  var nodeEnter = gnodes.enter()
 					    .append('g')
 					    .attr("class","gnode");
-					
+
 					  nodeEnter.append("circle")
 					    .attr("class", "node")
 					    .attr("id", function(d, i) { return d.name; })
 					    .attr("cx", function(d, i) { return d.x; })
 					    .attr("cy", function(d, i) { return d.y; })
 					    .attr("r", 14)
+					    .attr("stroke-width","2.5px")
+					    .attr("fill","#ffffff")
 					    .style("stroke", function(d, i) { return color(d.type); });
-					
+
 					  // Handling mouseover functions
-					  nodeEnter.selectAll(".node")
-					    .on("mousemove", function(d, i) {
-					      var mouse = d3.mouse(d3.select("body").node());
-					      tooltip
-					        .classed("hidden", false)
-					        .attr("class", "tooltip")
-					        .attr("style", "left:" + (mouse[0] + 20) + "px; top:" + (mouse[1] - 50) + "px")
-					        .html(tooltipText(d)); 
-					    });
+					  // nodeEnter.selectAll(".node")
+					  //   .on("mousemove", function(d, i) {
+					  //     var mouse = d3.mouse(d3.select("body").node());
+					  //     tooltip
+					  //       .classed("hidden", false)
+					  //       .attr("class", "tooltip")
+					  //       .attr("style", "left:" + (mouse[0] + 20) + "px; top:" + (mouse[1] - 50) + "px")
+					  //       .html(tooltipText(d));
+					  //   });
 					    // .on("mouseover", nodeOver);
-					
+
 					  nodeEnter.append("text")
 					    .style("text-anchor", "middle")
 					    .attr("dx", function(d) { return d.x; })
 					    .attr("dy", function(d) { return d.y + 5; })
-					    .text(function(d) { return d.token; });
-					
+					    .text(function(d) { return d.token; })
+					    .style("font","10px sans-serif");
+
 					  // d3.select("#trial2")
 					  //   .on("mouseover", trialOver);
 					}
-					
+
 					function drawLinks(links) {
 					  var radians = d3.scale.linear()
 					    .range([Math.PI / 2, 3 * Math.PI / 2]);
-					
+
 					  var arc = d3.svg.line.radial()
 					    .interpolate("basis")
 					    .tension(0)
 					    .angle(function(d) { return radians(d); });
-					
-					  d3.select("#plot").selectAll(".link")
+
+					  d3.select("#wrapper").selectAll(".link")
 					    .data(links)
 					  .enter().append("path")
 					    .attr("class", "link")
 					    .classed("highlighted", false)
-					    .attr("id", function(d) { return d.sessions})
-					    .style("stroke-width", function(d) { return (2 + d.pathCount); })
+					    .attr("id", function(d) { return d.sessions; })
 					    .attr("transform", function(d,i) {
 					      var xshift = d.source.x + (d.target.x - d.source.x) / 2;
 					      var yshift = yfixed;
@@ -227,78 +229,13 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					      radians.domain([0, points.length - 1]);
 					      return arc(points);
 					    })
-					    // .call(transition);
-					    // .on("mouseover", edgeOver);
+					    .attr("fill","none")
+					    .attr("stroke","#888888")
+					    .attr("stroke-weight","1px")
+					    .attr("stroke-opacity","0.5")
+					    .style("stroke-width", function(d) { return (2 + d.pathCount); });
 					}
-					
-					// Draw legend
-					//-----------------------------------------------------
-					function drawLegend(d) {
-					  var legend = svg.append("g")
-					      .attr("class", "legend");
-					  var key = legend.append("g")
-					
-					  // Initial
-					  key.append("circle")
-					      .attr("id", "legend_initial")
-					      .attr("cx", legend_x + key_x)
-					      .attr("cy", legend_y + key_y + 5)
-					      .attr("r", 5)
-					      .style("fill", "blue");
-					
-					    key.append("text")
-					      .attr("class", "legendText")
-					      .attr("id", "legend_initial_label")
-					      .attr("x", legend_x + key_x + 10 )
-					      .attr("y", legend_y + 10 + key_y )
-					      .text("Initial");
-					
-					    // Selection
-					    key.append("circle")
-					        .attr("id", "legend_selection")
-					        .attr("cx", function () { return legend_x + key_x })
-					        .attr("cy", function () { return legend_y + legend_margin + key_y + 5 })
-					        .attr("r", 5)
-					        .style("fill", "lightblue");
-					
-					    key.append("text")
-					        .attr("class", "legendText")
-					        .attr("id", "legend_selection_label")
-					        .attr("x", legend_x + key_x + 10)
-					        .attr("y", legend_y + legend_margin + 10 + key_y)
-					        .text("Selection");
-					
-					    // Final
-					    key.append("circle")
-					        .attr("id", "legend_final")
-					        .attr("cx", legend_x + key_x)
-					        .attr("cy", legend_y + 2 * legend_margin + key_y + 5)
-					        .attr("r", 5)
-					        .style("fill", "orange");
-					
-					    key.append("text")
-					        .attr("class", "legendText")
-					        .attr("id", "legend_final_label")
-					        .attr("x", legend_x + key_x + 10)
-					        .attr("y", legend_y + 2 * legend_margin + 10 + key_y)
-					        .text("Final");
-					
-					    // Delete
-					    key.append("circle")
-					        .attr("id", "legend_delete")
-					        .attr("cx", legend_x + key_x)
-					        .attr("cy", legend_y + 3 * legend_margin + key_y + 5)
-					        .attr("r", 5)
-					        .style("fill", "gold");
-					
-					    key.append("text")
-					        .attr("class", "legendText")
-					        .attr("id", "legend_delete_label")
-					        .attr("x", legend_x + key_x + 10)
-					        .attr("y", legend_y + 3 * legend_margin + 10 + key_y)
-					        .text("Delete");
-					}
-					
+
 					function tooltipText(d) {
 					 return "<h5>Information for " + d.token + "</h5>" +
 					   "<table>" +
@@ -324,47 +261,45 @@ angular.module('palladioPathView', ['palladio', 'palladio.services'])
 					   "</tr>" +
 					   "</table>";
 					}
-					
+
 					function segmentHighlight(streamHighlight) {
-					  if(d3.selectAll("path").filter(streamHighlight).classed("highlighted") == true){
-					    // d3.selectAll("path").filter(this !== streamHighlight).classed("suppressed", false)
-					    d3.selectAll("path").filter(streamHighlight).classed("highlighted", false)
+					  if(d3.selectAll("path").filter(streamHighlight).classed("highlighted") === true){
+					    d3.selectAll("path").filter(streamHighlight).classed("highlighted", false);
 					  } else {
-					    // d3.selectAll("path").filter(this !== streamHighlight).classed("suppressed", true)
-					    d3.selectAll("path").filter(streamHighlight).classed("highlighted", true)
+					    d3.selectAll("path").filter(streamHighlight).classed("highlighted", true);
 					  }
 					}
-					
+
 					// function resetHighlight() {
 					//   d3.selectAll("path").style("display","block");
 					// }
-					
+
 					function nodeOver(d,i) {
 					  d3.selectAll("path").style("stroke", function (p) {return p.source == d || p.target == d ? "#17becf" : "#888888"})
 					}
-					
+
 					function edgeOver(d) {
 					  d3.selectAll("path").style("stroke", function(p) {return p == d ? "#17becf" : "#888888"})
 					}
-					
+
 					function transition(path) {
 					  path.transition()
 					    .duration(2500)
 					    .attrTween("stroke-dasharray", tweenDash)
 					    .each("end", function() { d3.select(this).call(transition); });
 					}
-					
+
 					function tweenDash() {
 					  var l = this.getTotalLength(),
 					      i = d3.interpolateString("0," + l, l + "," + l);
 					    return function(t) { return i(t); };
 					}
-					
+
 					arcDiagram(scope.hardCodedData);
 				}
 			}
 		};
-		
+
 		function hardCodedData() {
 			return {
 				"nodes":[
